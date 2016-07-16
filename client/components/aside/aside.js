22aaa389ee450f81f0495f29d8847bea1959d394
Charles Yong <charles-yau@live.com.my>
2016-07-16 17:33:58 +0800

    aside change

--- client/components/aside/aside.js
+++ client/components/aside/aside.js
@@ -1,5 +1,28 @@
 import styles from './aside.css';
 
-Template.asideNav.helpers({
-    styles: styles
-});
+if (Meteor.isClient) {
+    Template.registerHelper('styles', function() {
+        // code
+       return styles;
+    });
+
+    Template.asideNav.events({
+        'submit #filter-form': function(e) {
+            e.preventDefault();
+            this.collegeName = e.target.cbxCollege.value;
+            this.courseCategory = e.target.cbxCourseCategory.value;
+            this.courseName = e.target.txtCourseName.value;
+            this.eduLevel = e.target.txtEduLevel.value;
+            this.location = e.target.txtLocation.value;
+            this.price = e.target.txtPrice.value;
+            this.rating = e.target.txtRating.value;
+            console.log(this);
+        }
+    });
+
+    Template.asideNav.onRendered( function() {
+        //debugger
+
+    });
+}
+
