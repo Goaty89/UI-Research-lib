51653686622d5580e7e6c37dd3d050f2774c4f7c
charles <charles-yau@live.com.my>
2016-07-17 18:22:33 +0800

    change price range and rating system.

--- client/components/aside/aside.js
+++ client/components/aside/aside.js
@@ -1,9 +1,52 @@
 import styles from './aside.css';
-
+const MAXPRICE = 1000.00;
+const MINPRICE = 0.00;
 if (Meteor.isClient) {
+
+    // slider starts at 0 and 100
+    Session.setDefault("slider", ["RM " + MINPRICE, "RM " + MAXPRICE]);
+    Template.PriceFilter.rendered = function() {
+
+        this.$("#slider").noUiSlider({
+            start: Session.get("slider"),
+            step: 10, // Slider moves in increments of '10'
+            margin: 20, // Handles must be more than '20' apart
+            connect: true,
+            behaviour: 'tap-drag', // Move handle on tap, bar is draggable
+            range: {
+                'min': MINPRICE,
+                'max': MAXPRICE
+            },
+            pips: {
+                mode: 'range',
+                density: 3
+            },
+            format: wNumb({
+                decimals: 2,
+                thousand: ',',
+                prefix: 'RM ',
+            })
+        }).on('slide', function(ev, val) {
+            // set real values on 'slide' event
+            Session.set('slider', val);
+        }).on('change', function(ev, val) {
+            // round off values on 'change' event
+            Session.set('slider', [val[0], val[1]]);
+        }).on('update', function(val, handle) {
+            // round off values on 'update' event
+            Session.set('slider', [val[handle], val[handle]]);
+        });
+    };
+
+    Template.PriceFilter.helpers({
+        slider: function() {
+            return Session.get("slider");
+        }
+    });
+
     Template.registerHelper('styles', function() {
         // code
-       return styles;
+        return styles;
     });
 
     Template.asideNav.events({
@@ -14,15 +57,16 @@ if (Meteor.isClient) {
             this.courseName = e.target.txtCourseName.value;
             this.eduLevel = e.target.txtEduLevel.value;
             this.location = e.target.txtLocation.value;
-            this.price = e.target.txtPrice.value;
-            this.rating = e.target.txtRating.value;
+            this.price = Session.get("slider");
+            this.rating = $('#rating').data('userrating');
+
+
             console.log(this);
         }
     });
 
-    Template.asideNav.onRendered( function() {
+    Template.asideNav.onRendered(function() {
         //debugger
 
     });
 }
-
