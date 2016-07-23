af22835e2b5559ac888dbc471c22d95f141b181f
charles <charles-yau@live.com.my>
2016-07-24 01:45:19 +0800

    add new file

---
+++ client/components/constants/FormData.js
@@ -0,0 +1,85 @@
+export const MONTH_LIST = [{
+        "key": "jan",
+        "label": "January"
+    }, {
+        "key": "feb",
+        "label": "February"
+    }, {
+        "key": "mar",
+        "label": "March"
+    }, {
+        "key": "apr",
+        "label": "April"
+    }, {
+        "key": "may",
+        "label": "May"
+    }, {
+        "key": "jun",
+        "label": "June"
+    }, {
+        "key": "jul",
+        "label": "July"
+    }, {
+        "key": "aug",
+        "label": "August"
+    }, {
+        "key": "sep",
+        "label": "September"
+    }, {
+        "key": "oct",
+        "label": "Octorber"
+    }, {
+        "key": "nov",
+        "label": "November"
+    }, {
+        "key": "dec",
+        "label": "December"
+    }
+
+];
+
+export const QUALIFICATION_LIST = [
+    { key: "certificate", label: "Certificate" }, 
+    { key: "PHD", label: "PHD" }, 
+    { key: "masters", label: "Masters" }, 
+    { key: "diploma", label: "Diploma" }, 
+    { key: "foundation", label: "Foundation" },
+    { key: "bachelor", label: "Bachelor" }
+];
+
+export const DELIVERY_MODE_LIST = [{
+                key: "online",
+                label: "Online"
+            }, {
+                key: "lecturer",
+                label: "Lecturer"
+            }, {
+                key: "collaborating",
+                label: "Collaborating"
+            }, {
+                key: "selfpaced",
+                label: "Self-Paced"
+            }];
+
+export const STUDY_MODE_LIST = [{
+                key: "fulltime",
+                label: "Full Time"
+            }, {
+                key: "parttime",
+                label: "Part Time"
+            }];
+
+export const PROVIDER_LIST = [
+    {key: "MMU", label: "Multimedia University (MMU)"},
+    {key: "UNITAR", label: "UNITAR"},
+    {key: "MUST", label: "Malaysia University of Science & Technology (MUST)"},
+    {key: "KDU", label: "KDU University College & Colleges (KDU)"},
+    {key: "UTAR", label: "Universiti Tunku Abdul Rahman (UTAR)"}
+];
+
+export const CATEGORY_LIST = [
+     {key: "any", label: "Any"},
+     {key: "computerscience", label: "Computer Science"},
+     {key: "informationsystem", label: "Information System"}
+
+];
