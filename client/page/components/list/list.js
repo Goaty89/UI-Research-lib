f265f5d39d2cc89f0e1ea1324beb886f23f603b1
charles <charles-yau@live.com.my>
2016-07-28 01:17:50 +0800

    add selected compare list while clicked on the check button.

--- client/page/components/list/list.js
+++ client/page/components/list/list.js
@@ -1,67 +1,75 @@
 import styles from './list.css';
 
+var COURSE_LIST = [{
+    id: "c1",
+    courseName: "Bachelor Degree in Computer Science",
+    rating: [1, 2, 3, 4, 5],
+    schoolName: "Multimedia University (MMU)",
+    location: "Cyberjaya",
+    minFees: "30,000",
+    maxFees: "60,000"
+}, {
+    id: "c2",
+    courseName: "Bachelor Degree in Information Systems",
+    rating: [1, 2, 3, 4],
+    schoolName: "Multimedia University (MMU)",
+    location: "Melaka",
+    minFees: "30,000",
+    maxFees: "60,000"
+}, {
+    id: "c3",
+    courseName: "Bachelor Degree in Information Technology",
+    rating: [1, 2, 3],
+    schoolName: "Multimedia University (MMU)",
+    location: "Cyberjaya",
+    minFees: "30,000",
+    maxFees: "60,000"
+}, {
+    id: "c4",
+    courseName: "Bachelor Degree in Psychology",
+    rating: [1, 2, 3, 4, 5],
+    schoolName: "Multimedia University (MMU)",
+    location: "Melaka",
+    minFees: "30,000",
+    maxFees: "60,000"
+}];
+var selectedCompareList = [];
+
 Template.list.helpers({
     styles: styles,
-    courseList:function()
-    {
-        var courseList = [
-          {
-            id:"c1",
-            courseName:"Bachelor Degree in Computer Science",
-            rating: [1,2,3,4,5],
-            schoolName: "Multimedia University (MMU)",
-            location: "Cyberjaya",
-            minFees: "30,000",
-            maxFees: "60,000"
-          },
-          {
-            id:"c2",
-            courseName:"Bachelor Degree in Information Systems",
-            rating: [1,2,3,4],
-            schoolName: "Multimedia University (MMU)",
-            location: "Melaka",
-            minFees: "30,000",
-            maxFees: "60,000"
-          },
-          {
-            id:"c3",
-            courseName:"Bachelor Degree in Information Technology",
-            rating: [1,2,3],
-            schoolName: "Multimedia University (MMU)",
-            location: "Cyberjaya",
-            minFees: "30,000",
-            maxFees: "60,000"
-          },
-          {
-            id:"c4",
-            courseName:"Bachelor Degree in Psychology",
-            rating: [1,2,3,4,5],
-            schoolName: "Multimedia University (MMU)",
-            location: "Melaka",
-            minFees: "30,000",
-            maxFees: "60,000"
-          }
-        ];
-        return courseList;
+    courseList: function() {
+        return COURSE_LIST;
     }
 });
 
-
 Template.list.events({
-  'click #btnCompare':function(evt, res)
-  {
-    evt.preventDefault();
-    var Item = $(evt.currentTarget.childNodes[0]);
-    if(Item.hasClass("fa-check-circle-o")){
-      Item.removeClass("fa-check-circle-o");
-      Item.addClass("fa-check-circle");
-      Item.css( "color", "#4EB947");
+    'click #compare-link': function(evt, res) {
+        evt.preventDefault();
+        var currentId = $(evt.currentTarget).data().id;
+        if (selectedCompareList.indexOf(currentId) > -1) {
+            selectedCompareList = selectedCompareList.filter(function(selectedIds) {
+                return selectedIds != currentId
+            });
+        } else {
+            selectedCompareList.push(currentId);
+        }
+        console.log(selectedCompareList);
+        var Item = $(evt.currentTarget).find("i");
+        if (Item.hasClass("fa-check-circle-o")) {
+            Item.removeClass("fa-check-circle-o");
+            Item.addClass("fa-check-circle");
+            Item.css("color", "#4EB947");
+        } else {
+            Item.removeClass("fa-check-circle");
+            Item.addClass("fa-check-circle-o");
+            Item.css("color", "#8A8A8A");
+        }
+        //console.log(Item);
     }
-    else {
-      Item.removeClass("fa-check-circle");
-      Item.addClass("fa-check-circle-o");
-      Item.css( "color", "#8A8A8A");
+});
+
+Template.btnCompare.events({
+    "click #btnCompare": function(evt, res) {
+        Router.go('courseComparison');
     }
-    console.log(Item);
-  }
 });
