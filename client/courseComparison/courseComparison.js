f683134f2e1648847c4bc47540b04808fe7c38e1
charles <charles-yau@live.com.my>
2016-08-02 00:15:56 +0800

    Add alittle bit compare list.

--- client/courseComparison/courseComparison.js
+++ client/courseComparison/courseComparison.js
@@ -1,9 +1,25 @@
+import styles from './courseComparison.css';
+
 Template.courseComparison.created = function() {
     console.log(Session.get("selectedCompareList"));
 }
 
 Template.cardContainer.created = function() {
-    if (Session.get("selectedCompareList")) {
+    let compareList = Session.get("selectedCompareList");
 
+    if (compareList) {
+        let detailList = Course.find({
+            _id: {
+                $in: compareList
+            }
+        }).fetch();
+        Session.set("courseDetails", detailList);
     }
 }
+
+Template.cardContainer.helpers({
+    styles: styles,
+    courseList: function() {
+        return Session.get("courseDetails");
+    }
+});
