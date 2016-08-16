3207812dc696080abe0c63971b6d59c3e0deb1e9
Jun Kit <jkchan@seekasia.com>
2016-08-16 17:28:32 +0800

    paging

--- client/page/components/list/list.js
+++ client/page/components/list/list.js
@@ -7,6 +7,7 @@ import {
 } from '../../../components/constants/map';
 
 if (Meteor.isClient) {
+    var MAX_COLUMN_PER_PAGE = 6;
     Template.list.onCreated(function() {
         Meteor.subscribe('allCourses', function() {
         });
@@ -19,73 +20,107 @@ if (Meteor.isClient) {
 
     Template.list.helpers({
         styles: styles,
-        courseList: function() {
-            var searchCriteria = Session.get("searchCriteria");
-            console.log("searchCriteria in list page");
-            console.log(searchCriteria);
+        allCoursesList:function(){
+          var searchCriteria = Session.get("searchCriteria");
 
-            var searchObject = {};
-            if(searchCriteria)
-            {
-              if(searchCriteria.collegeName.length>0){
-                searchObject.provider = {$in:searchCriteria.collegeName}
-              }
-              if(searchCriteria.courseCategory != "ANY"){
-                searchObject.qualificationCategory = searchCriteria.courseCategory;
-              }
-              if(searchCriteria.courseName != ""){
-                searchObject.qualification = {$regex:searchCriteria.courseName.toUpperCase()};
-              }
-              if(searchCriteria.location != ""){
-                searchObject.location = {$elemMatch:{"text":{$regex:searchCriteria.location}}};
-              }
-              if(searchCriteria.courseDuration != ""){
-                searchObject.courseDurationMin = {$lte: (searchCriteria.courseDuration * 48) }
-              }
-              if(searchCriteria.intakeMonth != "")
-              {
-                var month = MONTH_MAP[searchCriteria.intakeMonth];
-                searchObject.intake = {$elemMatch:{"month":MONTH_MAP[searchCriteria.intakeMonth]}};
-              }
+          var searchObject = {};
+          if(searchCriteria)
+          {
+            if(searchCriteria.collegeName.length>0){
+              searchObject.provider = {$in:searchCriteria.collegeName}
             }
-
-            var courses = [];
-            if(searchObject != {})
-            {
-              console.log(searchObject);
-              courses = Course.find(searchObject).fetch();
+            if(searchCriteria.courseCategory != "ANY"){
+              searchObject.qualificationCategory = searchCriteria.courseCategory;
             }
+            if(searchCriteria.courseName != ""){
+              searchObject.qualification = {$regex:searchCriteria.courseName.toUpperCase()};
+            }
+            if(searchCriteria.location != ""){
+              searchObject.location = {$elemMatch:{"text":{$regex:searchCriteria.location}}};
+            }
+            if(searchCriteria.courseDuration != ""){
+              searchObject.courseDurationMin = {$lte: (searchCriteria.courseDuration * 48) }
+            }
+            if(searchCriteria.intakeMonth != "")
+            {
+              var month = MONTH_MAP[searchCriteria.intakeMonth];
+              searchObject.intake = {$elemMatch:{"month":MONTH_MAP[searchCriteria.intakeMonth]}};
+            }
+          }
 
-            for(var course in courses){
-              var  c = courses[course];
-              c.rating = [1,2,3,4,5];
-              if(c.courseDurationMax == c.courseDurationMin)
+          var courses = [];
+          if(searchObject != {})
+          {
+            courses = Course.find(searchObject).fetch();
+          }
+          return courses;
+        },
+        courseList: function(courses) {
+            var returnCourses = [];
+            var start = Session.get("pageNum")?Session.get("pageNum")-1:0;
+
+            for(var course = start; course < courses.length;course++){
+              if(returnCourses.length<MAX_COLUMN_PER_PAGE)
               {
-                if(c.courseDurationMin>=48)
+                var  c = courses[course];
+                c.rating = [1,2,3,4,5];
+                if(c.courseDurationMax == c.courseDurationMin)
                 {
-                  c.courseDurationDisplay = (c.courseDurationMin/48) + " year(s)";
+                  if(c.courseDurationMin>=48)
+                  {
+                    c.courseDurationDisplay = (c.courseDurationMin/48) + " year(s)";
+                  }
+                  else{
+                    c.courseDurationDisplay = c.courseDurationMin + " week(s)"
+                  }
                 }
                 else{
-                  c.courseDurationDisplay = c.courseDurationMin + " week(s)"
+                  if(c.courseDurationMax>=48 && c.courseDurationMin>=48)
+                  {
+                    c.courseDurationDisplay = (c.courseDurationMin/48) + " ~ " + (c.courseDurationMax/48) + " year(s)";
+                  }
+                  else {
+                    c.courseDurationDisplay = c.courseDurationMin + " ~ " + c.courseDurationMax + " week(s)";
+                  }
                 }
-              }
-              else{
-                if(c.courseDurationMax>=48 && c.courseDurationMin>=48)
-                {
-                  c.courseDurationDisplay = (c.courseDurationMin/48) + " ~ " + (c.courseDurationMax/48) + " year(s)";
-                }
-                else {
-                  c.courseDurationDisplay = c.courseDurationMin + " ~ " + c.courseDurationMax + " week(s)";
-                }
+                returnCourses.push(c);
               }
             }
-            return courses;
+            return returnCourses;
         },
         setLocation: function(location) {
             var locationString = location.map(function(obj) {
                 return obj.text
             });
             return locationString.toString();
+        },
+        setPaging: function(list)
+        {
+          if(list.length<MAX_COLUMN_PER_PAGE){
+            return [{page:1}];
+          }
+          else {
+            var page = (list.length)/MAX_COLUMN_PER_PAGE;
+            var mod = (list.length)%MAX_COLUMN_PER_PAGE;
+            page = (mod==0)?page:page+1;
+            var returnValue = [];
+            for(var i = 0;i < page; i++)
+            {
+              returnValue.push({page:i+1});
+            }
+            return returnValue;
+          }
+        },
+        pageNumber:function(num)
+        {
+          if(Router.current().params.query.page && Router.current().params.query.page != num)
+          {
+            return false;
+          }
+          else
+          {
+            return true;
+          }
         }
     });
 
@@ -121,10 +156,11 @@ if (Meteor.isClient) {
         },
         "click #learnMore":function(evt, res){
           evt.preventDefault();
-          console.log(evt.currentTarget.name);
-
-          console.log("click with id value ::"+evt.currentTarget.name)
           Router.go("detailPage",{},{query:"id="+evt.currentTarget.name});
+        },
+        "click #btnPage":function(evt, res){
+          evt.preventDefault();
+          Session.set("pageNum",evt.currentTarget.name);
         }
     });
 
