04939f60add2feffdc382843264d61e7a2dd0252
Charles Yong <charles-yau@live.com.my>
2016-08-19 19:03:07 +0800

    add login

--- client/login/login.js
+++ client/login/login.js
@@ -1,14 +1,27 @@
-Template.Login.events({
-    'submit form': function(event) {
-        event.preventDefault();
-        var email = $('[name=email]').val();
-        var password = $('[name=password]').val();
-        Meteor.loginWithPassword(email, password, function(error) {
-            if (error) {
-                console.log(error.reason);
-            } else {
-                Router.go("register");
-            }
-        });
-    }
-});
+if (Meteor.isClient) {
+    Template.login.helpers({
+        isLogin: function() {
+            return !!Meteor.userId();
+        }
+    });
+
+    Template.login.events({
+        'submit form': function (event) {
+            event.preventDefault();
+            var email = $('[name=email]').val();
+            var password = $('[name=password]').val();
+            Meteor.loginWithPassword(email, password, function (error) {
+                if (error) {
+                    console.log(error.reason);
+                } else {
+                    Router.go("register");
+                }
+            });
+        },
+        'click .logout': function(event){
+            event.preventDefault();
+            Meteor.logout();
+            Router.go('login');
+        }
+    });
+}
--- client/register/register.js
+++ client/register/register.js
@@ -1,37 +1,64 @@
-Template.register.events({
-    'submit form': function(event) {
-        event.preventDefault();
-        var email = $('[name=email]').val();
-        var password = $('[name=password]').val();
-        const userDetail = {
-            email: email
-        };
+import { QUALIFICATION_LIST } from '../components/constants/FormData';
+if (Meteor.isClient) {
+    Template.register.onRendered(function() {
+        if(Meteor.userId()){
+            Router.go('login');
+        }
+    });
 
-        console.log("Email & password: ", email, password);
-        Accounts.createUser({
-            email: email,
-            password: password
-        }, function(error) {
-            if (error) {
-                console.log(error.reason); // Output error if registration fails
-            } else {
-                //login 
-                Meteor.loginWithPassword(email, password, function(error) {
-                    if (error) {
-                        console.log(error.reason);
-                    } else {
-                        userDetail.userId = Meteor.userId();
-                        Meteor.call('insertToUser_data', userDetail, function(error, result) {
-                            if (error) {
-                                console.log(error);
-                            }
-                        });
-                        console.log(Meteor.userId());
-                        //Router.go("register");
-                    }
-                });
-            }
-        });
-        //Router.go('login');
-    }
-});
+    Template.register.helpers({
+        notLogin: function(){
+            return !Meteor.userId();
+        }
+    });
+
+    Template.register.events({
+        'submit form': function (event) {
+            event.preventDefault();
+            var email = $('[name=email]').val();
+            var fname = $('[name=fname]').val();
+            var phone = $('[name=phoneNumber]').val();
+            var qualification = $('[name=cbxQualificationForSignup]').val();
+            var password = (phone && !!(+phone.replace(/ /g,''))) ? phone.substr(phone.length-4): "";
+            const userDetail = {
+                email: email,
+                fname: fname,
+                phone: phone,
+                qualification: qualification
+            };
+
+            console.log("Email & password: ",fname,phone,qualification, email, password);
+            Accounts.createUser({
+                email: email,
+                password: password
+            }, function (error) {
+                if (error) {
+                    console.log(error.reason); // Output error if registration fails
+                } else {
+                    //login
+                    Meteor.loginWithPassword(email, password, function (error) {
+                        if (error) {
+                            console.log(error.reason);
+                        } else {
+                            userDetail.userId = Meteor.userId();
+                            Meteor.call('insertToUser_data', userDetail, function (error, result) {
+                                if (error) {
+                                    console.log(error);
+                                }
+                            });
+                            console.log("Logined!");
+                            Router.go('login');
+                        }
+                    });
+                }
+            });
+            //Router.go('login');
+        }
+    });
+
+    Template.cbxQualificationForSignup.helpers({
+        List: function () {
+            return QUALIFICATION_LIST;
+        }
+    });
+}
